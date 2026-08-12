40c586d1c5cfb122a580e0219670425865dc3ce8
miles <buwryme@proton.me>
2026-08-12 13:42:28 +0100

    add use portal option to run exe thread

--- src/runexethread.py
+++ src/runexethread.py
@@ -2,15 +2,17 @@ from src.processthread import ProcessThread
 from src.utils import get_ae_install_dir
 
 class RunExeThread(ProcessThread):
-    def __init__(self, exe_args: list):
+    def __init__(self, exe_args: list, use_portal: bool = False):
         super().__init__()
         self.exe_args = exe_args
+        self.use_portal = use_portal
     
     def run(self):
         self.run_command(
             ['wine'] + self.exe_args, 
             cwd=get_ae_install_dir(),
-            in_prefix=True
+            in_prefix=True,
+            use_portal=self.use_portal
         )
 
         self.finished_signal.emit(True)
